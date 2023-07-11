72ff2fd5f3de69339f46c5c8a7f0f59c09ce5998
Vladislav <adwasar@Vladislavs-MacBook-Pro.local>
2023-07-11 20:02:16 +0300

    feat: form validation

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,35 +1,96 @@
+import { useForm } from 'react-hook-form';
+
 import styles from './Form.module.scss';
 
 function Form(props) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) =>
+    alert(
+      `name: ${data.name}, amount: ${data.amount}, transactionType: ${data.transactionType} date: ${data.date}`,
+    );
+
   return (
-    <form className={styles.form}>
-      <div className={styles['input-block']}>
-        <label htmlFor="transaction-name">Name:</label>
-        <input id="transaction-name" type="text" />
-      </div>
-      <div className={styles['input-block']}>
-        <label htmlFor="transaction-amount">Amount:</label>
-        <input id="transaction-amount" type="number" />
-      </div>
-      <div className={styles['input-radio-block']}>
-        <label htmlFor="transaction-type">Type:</label>
-        <div className={styles['radio-btns']}>
-          <div className={styles['radio-btn']}>
-            <input className={styles['transaction-type']} name="transaction-type" type="radio" />
-            <span>income</span>
-          </div>
-          <div className={styles['radio-btn']}>
-            <input className={styles['transaction-type']} name="transaction-type" type="radio" />
-            <span>expense</span>
+    <>
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+        <div className={styles['input-block']}>
+          <label htmlFor="transaction-name">Name:</label>
+          <input
+            {...register('name', { required: 'name is required field!' })}
+            id="transaction-name"
+            type="text"
+          />
+          {errors?.name && (
+            <div className={styles.error} style={{ color: 'red' }}>
+              {errors.name.message}
+            </div>
+          )}
+        </div>
+        <div className={styles['input-block']}>
+          <label htmlFor="transaction-amount">Amount:</label>
+          <input
+            {...register('amount', { required: 'amount is required field!' })}
+            id="transaction-amount"
+            type="number"
+          />
+          {errors?.amount && (
+            <div className={styles.error} style={{ color: 'red' }}>
+              {errors.amount.message}
+            </div>
+          )}
+        </div>
+        <div className={styles['input-radio-block']}>
+          <label htmlFor="transaction-type">Type:</label>
+          <div className={styles['radio-btns']}>
+            <div className={styles['radio-btn']}>
+              <input
+                {...register('transactionType', { required: 'please select a transaction type!' })}
+                id="income"
+                className={styles['transaction-type']}
+                name="transactionType"
+                value="income"
+                type="radio"
+              />
+              <label htmlFor="income">income</label>
+              {errors?.transactionType && (
+                <div className={styles.error} style={{ color: 'red' }}>
+                  {errors.transactionType.message}
+                </div>
+              )}
+            </div>
+            <div className={styles['radio-btn']}>
+              <input
+                {...register('transactionType', { required: 'please select a transaction type!' })}
+                id="expense"
+                className={styles['transaction-type']}
+                name="transactionType"
+                value="expense"
+                type="radio"
+              />
+              <label htmlFor="expense">expense</label>
+            </div>
           </div>
         </div>
-      </div>
-      <div className={styles['input-block']}>
-        <label htmlFor="transaction-date">Date:</label>
-        <input id="transaction-date" type="date" />
-      </div>
-      <input className={styles['submit-btn']} type="submit" />
-    </form>
+        <div className={styles['input-block']}>
+          <label htmlFor="transaction-date">Date:</label>
+          <input
+            {...register('date', { required: 'date is required field!' })}
+            id="transaction-date"
+            type="date"
+          />
+          {errors?.date && (
+            <div className={styles.error} style={{ color: 'red' }}>
+              {errors.date.message}
+            </div>
+          )}
+        </div>
+        <input className={styles['submit-btn']} type="submit" />
+      </form>
+    </>
   );
 }
 
