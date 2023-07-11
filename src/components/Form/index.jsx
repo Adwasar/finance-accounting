0e9e4d9b010140e83630445690c212f92dc27484
Vladislav <adwasar@Vladislavs-MacBook-Pro.local>
2023-07-11 12:41:32 +0300

    feat: type transaction btns

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -4,16 +4,27 @@ function Form(props) {
   return (
     <form className={styles.form}>
       <div className={styles['input-block']}>
-        <label htmlFor="transaction-name">Transaction name:</label>
+        <label htmlFor="transaction-name">Name:</label>
         <input id="transaction-name" type="text" />
       </div>
       <div className={styles['input-block']}>
-        <label htmlFor="transaction-amount">Transaction amount:</label>
+        <label htmlFor="transaction-amount">Amount:</label>
         <input id="transaction-amount" type="number" />
       </div>
       <div className={styles['input-block']}>
-        <label htmlFor="transaction-date">Transaction date:</label>
+        <label htmlFor="transaction-date">Date:</label>
         <input id="transaction-date" type="date" />
+      </div>
+      <div className={styles['input-radio-block']}>
+        <label htmlFor="transaction-type">Type:</label>
+        <div className={styles['radio-btn']}>
+          <input className={styles['transaction-type']} name="transaction-type" type="radio" />
+          <span>income</span>
+        </div>
+        <div className={styles['radio-btn']}>
+          <input className={styles['transaction-type']} name="transaction-type" type="radio" />
+          <span>expense</span>
+        </div>
       </div>
       <input className={styles['submit-btn']} type="submit" />
     </form>
